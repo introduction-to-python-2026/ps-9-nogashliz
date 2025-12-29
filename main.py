86235d85652365ae0a6d946eb49e083d7dce757e
nogashliz <nogashliz@gmail.com>
2025-12-29 16:35:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+# Download the data from your GitHub repository
+!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
+!wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
+import lab_setup_do_not_edit
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+import joblib
 
+df = pd.read_csv("parkinsons.csv")
+X = df[['PPE','DFA']]
+y = df['status']
+
+scaler = MinMaxScaler()
+X_scaled = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
+model = SVC()
+
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+
+print(f"Model Accuracy: {accuracy}")
+
+if accuracy >= 0.8:
+    print("Great! The accuracy is above 0.8.")
+else:
+    print("The accuracy is below 0.8, consider changing features or model parameters.")
+
+joblib.dump(model, 'my_model.joblib')
